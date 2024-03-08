862aeb43f50112f109b4ede2cc38bcfab992c405
vivek gupta <vivek8767804154@gmail.com>
2024-03-08 12:28:05 +0530

    Add Form Validation

--- src/main/java/com/vivek/Controller/ProductController.java
+++ src/main/java/com/vivek/Controller/ProductController.java
@@ -1,15 +1,16 @@
 package com.vivek.Controller;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.vivek.entity.Product;
 import com.vivek.repo.ProductRepo;
 
@@ -18,6 +19,14 @@ public class ProductController {
 	@Autowired
 	private ProductRepo repo;
 	
+	@GetMapping("/edit")
+	public String editData(@RequestParam("id") Integer id, Model model) {
+		  Optional<Product> byId = repo.findById(id);
+		  if(byId.isPresent()) {
+			  model.addAttribute("pr", byId.get());
+		  }
+		return "index";
+	}
 	@GetMapping("/delete")
 	public String deleteData(@RequestParam("id") Integer id ,Model model) {		
 		repo.deleteById(id);
@@ -26,20 +35,27 @@ public class ProductController {
 		return "data";
 	}
 	
+	
       @GetMapping("/view")
 	public String getAllData(Model model) { 
 		model.addAttribute("products", repo.findAll());
 		return "data";
 		
 	}
+      
 	@PostMapping("/product")
-	public String saveProduct(@ModelAttribute("pr") Product pr,Model model) {
+	public String saveProduct( @Validated @ModelAttribute("pr") Product pr,BindingResult result,Model model) {
+		 
+		if(result.hasErrors()) {
+			return "index";
+		}
 		repo.save(pr);
 		if(pr.getId() != null) {
 			model.addAttribute("msg", "Data Saved...");
 		}
 		return "index";
 	}
+	
 	@GetMapping("/")
    public  String loadPage(Model model) {
 	   model.addAttribute("pr", new Product());
--- src/main/java/com/vivek/entity/Product.java
+++ src/main/java/com/vivek/entity/Product.java
@@ -1,20 +1,31 @@
 package com.vivek.entity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 @Entity
 public class Product {
-	
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
   private  Integer id;
+    
+  @NotBlank(message = "Name is Required") 
+  @Size(min = 3 ,max = 10 , message = "Name Should be 3-10 Character")
   private String name;
+  
+  @NotNull(message="Price is Required")
+  @Positive(message="Price should be positive")
   private Double price;
+
+  @NotNull(message="quantity is Required")
+  @Positive(message="Price should be positive")
   private Integer quantity;
 	
 	
